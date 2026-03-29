2fe11a4b9d6f07310c1fa5b20f9e13ba0a8d9d72
Devesh Parmar <81907545+deveshparmar@users.noreply.github.com>
2026-03-29 12:24:50 +0530

    resolved stylings in portfolio

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./layout/navbar/navbar";
+import { Home } from "./features/home/home";
+import { Experience } from "./features/experience/experience";
+import { Projects } from "./features/projects/projects";
+import { Skill } from "./features/skill/skill";
+import { Contact } from "./features/contact/contact";
+import { About } from "./features/about/about";
+import { Footer } from "./layout/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, Home, Experience, Projects, Skill, Contact, About, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
